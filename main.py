6698b573f61acbccce9cd8732bc4927181e8e2e0
Tasneem Sh.Y <102421553+TasneemShy@users.noreply.github.com>
2024-04-15 12:11:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+from Square import Square
+from Rectangle import Rectangle
+from Triangle import Triangle
 from Circle import Circle
 from Hexagon import Hexagon
 
 def main():
+    s = Square(5)
+    r = Rectangle(2,10)
+    t = Triangle(6,3)
     c = Circle(5)
     h = Hexagon(3)
+
+    print(s)
+    print("Square area: ",s.get_area())
+    print(r)
+    print("Rectangle area: ",r.get_area())
+    print(t)
+    print("Triangle area: ",t.get_area())
     print(c)
     print("Circle area: ",c.get_area())
     print(h)
