01acb4a8a417ee96726fe49a028d16a988d75bcb
Tasneem Sh.Y <102421553+TasneemShy@users.noreply.github.com>
2024-04-20 01:35:09 +0300

    Update Rectangle.py
    input validation

--- Rectangle.py
+++ Rectangle.py
@@ -4,18 +4,25 @@ class Rectangle(Shape):
     """
     Rectangle shape class inheriting from Shape
     """
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
+    def __init__(self, height, width):
+        """
+        Constarts a Rectangle and checks the input.
+        """
+        if super().validate_input(height) and super().validate_input(width):
+            self.height = height
+            self.width = width        
+        else: 
+            self.height = 0
+            self.width = 0
 
     def get_area(self):
         """
         Method to calculate the area of the rectangle.
         """
-        return self.length * self.width
+        return self.height * self.width
 
     def __str__(self):
         """
         String casting for the Rectangle (In case of print for example)
         """
-        return f"{super().__str__()}, Length: {self.length}, Width: {self.width}, Area: {self.get_area()}"
+        return f"{super().__str__()}, Height: {self.height}, Width: {self.width}, Area: {self.get_area()}"
